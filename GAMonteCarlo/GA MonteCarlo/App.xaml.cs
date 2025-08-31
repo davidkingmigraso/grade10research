a56b94cb5d8d12ad76cf359a914881c22ac4b6a8
Dawud Abdul Malik Migraso <davidking.migraso@gmail.com>
2025-08-31 17:43:34 +0800

    add ui

---
+++ GAMonteCarlo/GA MonteCarlo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GA_MonteCarlo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
